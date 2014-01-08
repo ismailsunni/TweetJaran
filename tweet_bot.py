2274385e9696adf97346c1887ad1edb48c12acc9
Ismail Sunni <imajimatika@gmail.com>
2014-01-08 12:17:49 +0900

    Done.

--- tweet_bot.py
+++ tweet_bot.py
@@ -5,10 +5,18 @@ __author__ = '@ismailsunni'
 
 
 import tweepy
-import  constants
+import constants
 
 # constant
 consumer_key = constants.consumer_key
 consumer_secret = constants.consumer_secret
 access_key = constants.access_key
 access_secret = constants.access_secret
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_key, access_secret)
+api = tweepy.API(auth)
+
+# print api.me().name
+# print api.me()
+api.update_status('Sedang gundah apa hari ini?')
