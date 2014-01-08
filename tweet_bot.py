52cb4569fd6c4042d042728a42a7cb95e72f5455
Ismail Sunni <imajimatika@gmail.com>
2014-01-08 11:53:50 +0900

    Initial commit

---
+++ tweet_bot.py
@@ -0,0 +1,14 @@
+"""Tweet Bot, for tweeting random tweet
+"""
+__date__ = '08/01/2014'
+__author__ = '@ismailsunni'
+
+
+import tweepy
+import  constants
+
+# constant
+consumer_key = constants.consumer_key
+consumer_secret = constants.consumer_secret
+access_key = constants.access_key
+access_secret = constants.access_secret
